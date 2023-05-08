802298f888f4bd50d697df21ac5abeb5b5aa4c4b
zendo <linzway@qq.com>
2023-05-09 00:33:38 +0800

    fedora codecs

--- fedora-setup-dialog.sh
+++ fedora-setup-dialog.sh
@@ -15,10 +15,11 @@ OPTIONS=(1 "Debloat System"
          2 "Speeding Up DNF"
          3 "Enabling RPM Fusion"
          4 "Installing Essential Software"
-         5 "Installing Extras"
-         6 "Install Nvidia - Install akmod nvidia drivers"
-         7 "Disable SElinux - Reboot need"
-         8 "Quit")
+         5 "Installing Codecs"
+         6 "Installing Extras"
+         7 "Install Nvidia - Install akmod nvidia drivers"
+         8 "Disable SElinux - Reboot need"
+         9 "Quit")
 
 while [ "$CHOICE -ne 4" ]; do
     CHOICE=$(dialog --clear \
@@ -57,28 +58,32 @@ while [ "$CHOICE -ne 4" ]; do
             notify-send "Software has been installed" --expire-time=10
             ;;
 
-        5)  echo "Installing Extras"
-            # sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
-            # sudo dnf install -y lame\* --exclude=lame-devel
+        5)  echo "Installing Codecs"
+            sudo dnf group upgrade -y --with-optional --allowerasing Multimediar
+            # sudo dnf install lame\* --exclude=lame-devel
+            notify-send "Codecs has been installed" --expire-time=10
+            ;;
+
+
+        6)  echo "Installing Extras"
             sudo dnf copr enable zhullyb/v2rayA -y
-            sudo dnf install -y @multimedia
             sudo dnf group install -y --with-optional virtualization
             sudo dnf group install -y @"C Development Tools and Libraries"
             notify-send "All done" --expire-time=10
             ;;
 
-        6)  echo "Installing Nvidia Driver Akmod-Nvidia"
+        7)  echo "Installing Nvidia Driver Akmod-Nvidia"
             sudo dnf install -y akmod-nvidia
             notify-send "All done" --expire-time=10
             ;;
 
-        7)  echo "Disable SElinux"
+        8)  echo "Disable SElinux"
             sudo sed -i s/^SELINUX=.*$/SELINUX=disabled/ /etc/selinux/config
             sestatus
             notify-send "All done" --expire-time=10
             ;;
 
-        8)
+        9)
             exit 0
             ;;
     esac
